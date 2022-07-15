c80ce9dea3bb8dcac4f57de7901dc8b4660a0966
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-15 13:42:13 +0900

    Add files via upload

---
+++ question05_4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct {
+	int data1;
+	int data2;
+	int data3;
+}Data;
+
+int main() {
+	Data d;
+	Data* pd;
+
+	pd = &d;
+
+	printf("d의 크기: %d\n", sizeof(d));
+	printf("pd의 크기: %d\n", sizeof(pd));
+
+	printf("\npd의 값: %p\n", pd);
+
+	return 0;
+}
