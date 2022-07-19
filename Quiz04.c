f6e21a8797b9ca1d3358622d4be09f0ec87d2c08
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-19 14:17:00 +0900

    Add files via upload

---
+++ Quiz04.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	char* name;
+	int score;
+}Element;
+
+typedef struct {
+	int data1;
+	int data2;
+	Element* p;
+}Data1;
+
+int main()
+{
+	Data1* p = (Data1*)malloc(sizeof(Data1));
+	p->p = (Element*)malloc(sizeof(Element));
+	p->p->name = (char*)malloc(sizeof(char) * 20);
+
+	printf("이름을 입력하세요 : ");
+	scanf("%s", p->p->name);
+	//scanf()를 이용해 name에 이름을 입력 받으세요.
+
+	printf("점수를 입력하세요 : ");
+	scanf("%d", &p->p->score);
+	//scanf()를 이용해 score에 점수를 입력 받으세요.
+	
+	printf("이름은: %s, 점수는: %d 점이다.", p->p->name, p->p->score);
+	//입력받은 값을 출력하세요.
+	
+	free(p->p->name);
+	free(p->p);
+	free(p);
+	//동적 해제 코드를 추가하세요.
+
+	return 0;
+}
